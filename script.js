7a0d7bb709b4e0232a63709fe20ae5a81e6ffcb1
Muhammad-Noman-g <noman70819@gmail.com>
2025-11-09 21:34:46 +0500

    JS Done

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
-const input = document.querySelector("#input-btn-div");
+const inputDiv = document.querySelector("#input-btn-div");
+const input = document.querySelector("#other-hobbies-input");
+
 const other = document.querySelector("#other");
 const checkboxGroup = document.querySelector(".checkbox-group");
+const inputBtn = document.querySelector("#input-btn-div button");
+
 other.addEventListener("change", function () {
-  input.style.display = this.checked ? "flex" : "none";
+  inputDiv.style.display = this.checked ? "flex" : "none";
 });
 
+inputBtn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  addHobby();
+});
 function addHobby() {
   let hobby = input.value;
-  console.log(hobby);
+  let newAlignDiv = document.createElement("div");
+  newAlignDiv.setAttribute("class", "align");
+  let newInput = document.createElement("input");
+  newInput.setAttribute("type", "checkbox");
+  newInput.setAttribute("name", "hobbies");
+  newInput.setAttribute("id", hobby.toLowerCase());
+  newInput.setAttribute("value", hobby);
+  newInput.checked = "checked";
+  newAlignDiv.append(newInput);
+  let newLabel = document.createElement("label");
+  newLabel.setAttribute("for", hobby.toLowerCase());
+  newLabel.innerText = hobby;
+  newAlignDiv.append(newLabel);
+  let index = checkboxGroup.children.length - 2;
+  checkboxGroup.insertBefore(newAlignDiv, checkboxGroup.children[index]);
+  input.value = "";
+  other.checked = false;
+  other.dispatchEvent(new Event("change"));
 }
