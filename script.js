44f5a21d2ca78cacf9589cfd32046a39875e45e8
Muhammad-Noman-g <noman70819@gmail.com>
2025-11-10 08:04:42 +0500

    Final done

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const inputDiv = document.querySelector("#input-btn-div");
 const input = document.querySelector("#other-hobbies-input");
-
 const other = document.querySelector("#other");
 const checkboxGroup = document.querySelector(".checkbox-group");
 const inputBtn = document.querySelector("#input-btn-div button");
@@ -13,23 +12,29 @@ inputBtn.addEventListener("click", (evt) => {
   evt.preventDefault();
   addHobby();
 });
+
 function addHobby() {
   let hobby = input.value;
   let newAlignDiv = document.createElement("div");
   newAlignDiv.setAttribute("class", "align");
+
   let newInput = document.createElement("input");
   newInput.setAttribute("type", "checkbox");
   newInput.setAttribute("name", "hobbies");
   newInput.setAttribute("id", hobby.toLowerCase());
   newInput.setAttribute("value", hobby);
-  newInput.checked = "checked";
+  newInput.checked = true;
   newAlignDiv.append(newInput);
+
   let newLabel = document.createElement("label");
   newLabel.setAttribute("for", hobby.toLowerCase());
   newLabel.innerText = hobby;
   newAlignDiv.append(newLabel);
+
   let index = checkboxGroup.children.length - 2;
+
   checkboxGroup.insertBefore(newAlignDiv, checkboxGroup.children[index]);
+
   input.value = "";
   other.checked = false;
   other.dispatchEvent(new Event("change"));
